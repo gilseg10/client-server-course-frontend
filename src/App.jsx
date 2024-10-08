7c987ff7f4faca4029d9864c099fbb63043898b8
Lior Jigalo <you@example.com>
2024-10-08 15:22:16 +0300

    fixed routing issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
-import {Routes,Route} from 'react-router-dom'
-import Login from "./components/Login"
-import Signup from "./components/Signup"
-import MainPage from './components/MainPage'
-import Contactus from './components/Contactus'
+// import {HashRouter as Router, Routes, Route} from 'react-router-dom'
+// import Login from "./components/Login"
+// import Signup from "./components/Signup"
+// import MainPage from './components/MainPage'
+// import Contactus from './components/Contactus'
+//
+//
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="/contact-us" element={<Contactus />} />
+//         <Route path="/mainPage" element={<MainPage />} />
+//       </Routes>
+//     </>
+//   )
+// }
+//
+// export default App
+//
 
+
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import MainPage from './components/MainPage';
+import Contactus from './components/Contactus';
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/contact-us" element={<Contactus />} />
-        <Route path="/mainPage" element={<MainPage />} />
-      </Routes>
-    </>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/contact-us" element={<Contactus />} />
+                <Route path="/mainPage" element={<MainPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
